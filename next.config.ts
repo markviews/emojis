c2989a306e6c4f7bbd4fd7559d4975ba4e77693d
Marcus Olivas <markmark9788@gmail.com>
2025-01-15 11:18:54 -0800

    build without basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
-  basePath: '/emojis' // github repo name
 };
 
 export default nextConfig;
