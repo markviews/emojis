bf542bde3eb474a92278ca382c256033613af9f5
Marcus Olivas <markmark9788@gmail.com>
2025-01-15 11:04:01 -0800

    re-build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
+  basePath: '/emojis' // github repo name
 };
 
 export default nextConfig;
